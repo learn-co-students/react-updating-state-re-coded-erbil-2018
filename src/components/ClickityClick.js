f3f0fe02561f5c61cd942a698b8993cdd6ccc4b3
sumaya <sumaya.shahoyi@gmail.com>
2018-09-06 08:15:54 +0300

    add

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,10 @@
 // Code ClickityClick Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    Mount Components Here
+  </div>,
+  document.getElementById('root')
+);
